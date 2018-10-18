e828a6331a5dbddef915493841626a56251feee4
Fernanda Bruno <februno@gmail.com>
2018-10-17 23:00:22 -0300

    Update searchTrack.R
    Adjusting again

--- R/searchTrack.R
+++ R/searchTrack.R
@@ -16,18 +16,20 @@ searchTrack<-function(artist,song,apikey){
   song<-gsub(" ", "%20", song)
   data<-jsonlite::fromJSON(paste0(
     "http://api.musixmatch.com/ws/1.1/track.search?q_artist=",artist,"&q_track=",song,"&apikey=",apikey))
-
+  
+  
   metadata<-data.frame(track_id=data$message$body$track_list$track$track_id,
-                    nomes=data$message$body$track_list$track$track_name,
-                    track_rating=data$message$body$track_list$track$track_rating,
-                    track_length=data$message$body$track_list$track$track_length,
-                    track_has_lyrics=data$message$body$track_list$track$has_lyrics,
-                    track_richsync=data$message$body$track_list$track$has_richsync,
-                    track_numfavs=data$message$body$track_list$track$num_favourite,
-                    album_name=data$message$body$track_list$track$album_name,
-                    track_album_name=data$message$body$track_list$track$album_id,
-                    track_release=data$message$body$track_list$track$first_release_date,
-                    genre_list=unlist(data$message$body$track_list$track$primary_genres$music_genre_list))
+                       nomes=data$message$body$track_list$track$track_name,
+                       track_rating=data$message$body$track_list$track$track_rating,
+                       track_length=data$message$body$track_list$track$track_length,
+                       track_has_lyrics=data$message$body$track_list$track$has_lyrics,
+                       track_richsync=data$message$body$track_list$track$has_richsync,
+                       track_numfavs=data$message$body$track_list$track$num_favourite,
+                       album_name=data$message$body$track_list$track$album_name,
+                       track_album_name=data$message$body$track_list$track$album_id,
+                       track_release=data$message$body$track_list$track$first_release_date,
+                       genre_list=data$message$body$track_list$track$primary_genres$music_genre_list[[1]]$music_genre$music_genre_name,
+                       stringsAsFactors = FALSE)
   return(metadata)
   
 }
