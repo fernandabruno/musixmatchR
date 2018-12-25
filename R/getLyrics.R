e604da7d64c185ecf69314f5c747176a578365a4
Fernanda Bruno <februno@gmail.com>
2018-12-25 13:59:33 -0200

    Update getLyrics.R
    Updated function in order to reflect API recent changes.

--- R/getLyrics.R
+++ R/getLyrics.R
@@ -14,12 +14,9 @@ getLyrics<-function(id_track,apikey){
   metadata<-jsonlite::fromJSON(paste0(
     "http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=",id_track,"&apikey=",apikey))
 
-  tracks<-data.frame(track_id=id_track,
-                    lyrics_id=metadata$message$body$lyrics$lyrics_id,
-                    explicit=metadata$message$body$lyrics$explicit,
-                    instrumental=metadata$message$body$lyrics$instrumental,
-                    lyrics_body=gsub("[\r\n]", " ", metadata$message$body$lyrics$lyrics_body),
-                    lyrics_language=metadata$message$body$lyrics$lyrics_language_description)
-
+tracks<-data.frame(track_id=id_track,
+                     lyrics_id=metadata$message$body$lyrics$lyrics_id,
+                     explicit=metadata$message$body$lyrics$explicit,
+                     lyrics_body=gsub("[\r\n]", " ", metadata$message$body$lyrics$lyrics_body))
   return(tracks)
 }
